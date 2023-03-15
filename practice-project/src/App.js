ad0f3e7278ecd01af2b8aae28b50d805145672e0
msi\Phuong <abc@email>
2023-03-15 11:43:45 +0700

    practice project

--- practice-project/src/App.js
+++ practice-project/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import UserForm from './components/UserForm/Form';
 
 function App() {
+    const addUser = 
     return (
         <div className="App">
             <div class="container-fluid text-center">
                 <div class="row">
                     <div class="col"></div>
-                    <div class="col-6">
-                        <UserForm />
+                    <div class="col-5">
+                        <UserForm addUser={addUser} />
                     </div>
                     <div class="col"></div>
                 </div>
--- practice-project/src/components/UserForm/Form.js
+++ practice-project/src/components/UserForm/Form.js
@@ -4,21 +4,42 @@ import Form from 'react-bootstrap/Form';
 import styles from './Form.module.css';
 
 const UserForm = (props) => {
-    const [enterUserName, setEnterUserName] = useState('');
+    const [enterUsername, setEnterUsername] = useState('');
+    const [enterAge, setEnterAge] = useState('');
 
+    const addUser = (event) => {
+        event.preventDefault();
+        props.addUser(enterUsername, enterAge);
+        setEnterUsername('');
+        setEnterAge('');
+    };
+    const userChange = (event) => {
+        setEnterUsername(event.target.value);
+    };
+    const ageChange = (event) => {
+        setEnterAge(event.target.value);
+    };
     console.log('styles', styles);
     console.log('btn', styles.btn);
     console.log('pp', styles.purple);
 
     return (
-        <Form className="text-start rounded-2 bg-light">
-            <Form.Group className="mb-3 fw-bold" controlId="formBasicEmail">
+        <Form className="text-start rounded-2 bg-light p-4 mt-4">
+            <Form.Group className="mb-3 fw-bold" controlId="formBasicText">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="text" value={enterUserName} />
+                <Form.Control
+                    type="text"
+                    value={enterUsername}
+                    onChange={userChange}
+                />
             </Form.Group>
-            <Form.Group className="mb-3 fw-bold" controlId="formBasicPassword">
+            <Form.Group className="mb-3 fw-bold" controlId="formBasicNumber">
                 <Form.Label>Age (Years)</Form.Label>
-                <Form.Control type="number" />
+                <Form.Control
+                    type="number"
+                    value={enterAge}
+                    onChange={ageChange}
+                />
             </Form.Group>
             <Button
                 // className={styles['btn purple']}
